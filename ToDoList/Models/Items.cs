41ba8e0253c95f840d7c130d1fdd3f99b96d7584
bendunhampdx <bendunhampdx@gmail.com>
2021-09-22 10:03:35 -0700

    build ToDoList user interface
    Co-authored-by: Nathan Fletcher <nathan.fletcher@gmail.com>

---
+++ ToDoList/Models/Items.cs
@@ -0,0 +1,26 @@
+using System.Collections.Generic;
+
+
+namespace ToDoList.Models
+{
+  public class Item
+  {
+    public string Description { get; set;}
+    
+    public static List<Item> Instances { get; } = new List<Item> {};
+
+    public Item(string description)
+    {
+      Description = description;
+      Instances.Add(this); 
+    }
+    public static List<Item> GetAll()
+    {
+      return Instances;
+    }
+    public static void ClearAll()
+    {
+      Instances.Clear();
+    }
+  }
+}
